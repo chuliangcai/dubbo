18376ee1060aa67d189eb1aaf4b1f30fd5443c68
chuliangcai <790704125@qq.com>
2018-12-03 22:16:28 +0800

    Default Changelist

--- dd-provider/src/main/java/com/example/provider/Main.java
+++ dd-provider/src/main/java/com/example/provider/Main.java
@@ -1,5 +1,6 @@
 package com.example.provider;
 
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +10,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author superman
  */
 @SpringBootApplication
+@DubboComponentScan(basePackages = "com.example.provider.dubbo")
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new SpringApplicationBuilder(Main.class)
                 .web(WebApplicationType.NONE) // .REACTIVE, .SERVLET
                 .run(args);
+        System.in.read();
     }
 }
--- dd-provider/src/main/java/com/example/provider/dubbo/DubboConfiguration.java
+++ dd-provider/src/main/java/com/example/provider/dubbo/DubboConfiguration.java
@@ -1,7 +1,24 @@
 package com.example.provider.dubbo;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DubboConfiguration {
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("hello-world");
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("zookeeper://192.168.56.101:2181");
+        return registryConfig;
+    }
 }
